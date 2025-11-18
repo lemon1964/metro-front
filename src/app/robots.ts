2b9ea058056c442476be394ff639aaf22cf4870c
lemon <lemon@example.com>
2025-11-18 20:38:51 +0300

    commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,13 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  // const base = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
   return {
     rules: [
-      { userAgent: "*", allow: "/" },
+      { userAgent: '*', disallow: '/' }
+      // { userAgent: "*", allow: "/" },
     ],
-    sitemap: `${base}/sitemap.xml`,
-    host: base,
+    // sitemap: `${base}/sitemap.xml`,
+    // host: "https://metro-front.onrender.com"
   };
 }
